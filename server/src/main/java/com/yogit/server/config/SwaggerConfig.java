93028598a43b8bad72fb38ec1b719bcdf9fd17ae
xhaktmchl <xhaktmchl@naver.com>
2022-09-23 16:52:08 +0900

    #2 feat: userController생성 및 swagger설정파일 오류 수정

--- server/src/main/java/com/yogit/server/config/SwaggerConfig.java
+++ server/src/main/java/com/yogit/server/config/SwaggerConfig.java
@@ -1,35 +1,32 @@
 package com.yogit.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2//  기능을 활성화하는 어노테이션입니다. SwaggerConfig.java 가 아니라 어플리케이션에 추가해도 동작합니다.
 public class SwaggerConfig {
 
+    // 기본 swagger 선언
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.velog.eunjy.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Swagger Test")
-                .description("SwaggerConfig 요깃 API 명세서 입니다.")
-                .version("3.0")
                 .build();
     }
+
 }

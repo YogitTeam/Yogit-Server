29c019645dff94657a31db271a5a9d4516a2e7c8
xhaktmchl <xhaktmchl@naver.com>
2022-09-23 16:14:01 +0900

    feat: swaggerconfig 파일에 @EnableSwagger2 추가

--- server/src/main/java/com/yogit/server/config/SwaggerConfig.java
+++ server/src/main/java/com/yogit/server/config/SwaggerConfig.java
@@ -8,8 +8,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2//  기능을 활성화하는 어노테이션입니다. SwaggerConfig.java 가 아니라 어플리케이션에 추가해도 동작합니다.
 public class SwaggerConfig {
 
     @Bean
@@ -26,7 +28,7 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Swagger Test")
-                .description("SwaggerConfig")
+                .description("SwaggerConfig 요깃 API 명세서 입니다.")
                 .version("3.0")
                 .build();
     }

59b9ffe9917a9a8103b9b14b3fc1658c2655e221
xhaktmchl <xhaktmchl@naver.com>
2022-11-10 20:56:56 +0900

    #9 refactor: 보드 삭제API URL 변경

--- server/src/main/java/com/yogit/server/board/controller/BoardController.java
+++ server/src/main/java/com/yogit/server/board/controller/BoardController.java
@@ -106,7 +106,7 @@ public class BoardController {
             @ApiResponse(code= 404, message = "요청한 유저가 호스트가 아닙니다."),
             @ApiResponse(code = 4000 , message =  "서버 오류입니다.")
     })
-    @PatchMapping("/del")
+    @PatchMapping("/status")
     public ApplicationResponse<BoardRes> deleteBoard(@RequestBody @Validated DeleteBoardReq deleteBoardReq){
         return boardService.deleteBoard(deleteBoardReq);
     }

7fa82bfc10712a42f26a11ebe8a3e1c3b387324f
shinhn <rkdrpgml@naver.com>
2023-02-10 20:06:20 +0900

    #8 fix : 프로필 정보 등록/수정 - res 프로필 사진 수정, 관심사 추가 로직 변경

--- server/src/main/java/com/yogit/server/user/repository/InterestRepository.java
+++ server/src/main/java/com/yogit/server/user/repository/InterestRepository.java
@@ -3,5 +3,10 @@ package com.yogit.server.user.repository;
 import com.yogit.server.user.entity.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InterestRepository extends JpaRepository<Interest, Long> {
+    Interest findByName(String name);
+
+    boolean existsByName(String name);
 }

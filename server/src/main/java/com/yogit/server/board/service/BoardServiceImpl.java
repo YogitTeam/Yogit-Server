67c0e5eec324b787bcd40ca8dc64e4605a257b68
xhaktmchl <xhaktmchl@naver.com>
2023-03-09 15:46:26 +0900

    #9 feat: 보드 카테고리1별 조회API-차단한 유저의 보드 안보이게 적용

--- server/src/main/java/com/yogit/server/board/service/BoardServiceImpl.java
+++ server/src/main/java/com/yogit/server/board/service/BoardServiceImpl.java
@@ -299,6 +299,10 @@ public class BoardServiceImpl implements BoardService{
         User user = userRepository.findByUserId(dto.getUserId())
                 .orElseThrow(() -> new NotFoundUserException());
 
+        List<User> blockedUsers = blockRepository.findBlocksByBlockingUserId(dto.getUserId()).stream()
+                .map(block -> block.getBlockedUser())
+                .collect(Collectors.toList());
+
         // jpa 다중 정렬 order
         Sort sort = Sort.by(
                 Sort.Order.desc("id")
@@ -308,6 +312,7 @@ public class BoardServiceImpl implements BoardService{
         Page<Board> boards = boardRepository.findAllBoardsByCategory(pageRequest, dto.getCategoryId());
         //  보드 res에 이미지uuid -> aws s3 url로 변환
         List<GetAllBoardRes> boardsRes = boards.stream()
+                .filter(board -> !blockedUsers.contains(board.getHost())) // 차단당한 유저의 데이터 제외
                 .map(board -> GetAllBoardRes.toDto(board, awsS3Service.makeUrlOfFilename(board.getBoardImagesUUids().get(0)), awsS3Service.makeUrlOfFilename(board.getHost().getProfileImg())))
                 .collect(Collectors.toList());
 

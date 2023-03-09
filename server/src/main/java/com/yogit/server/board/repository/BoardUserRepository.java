cf7aa0bb8921679d967f2a5e3495644e35461c29
shinhn <rkdrpgml@naver.com>
2023-03-09 16:35:09 +0900

    #9 feat : 게시글 조회 - 모임에 참여한 유저 프로필 사진들 포함

--- server/src/main/java/com/yogit/server/board/repository/BoardUserRepository.java
+++ server/src/main/java/com/yogit/server/board/repository/BoardUserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +18,6 @@ public interface BoardUserRepository extends JpaRepository<BoardUser, Long> {
 
     @Query("select bu from BoardUser bu where bu.status = 'ACTIVE' and bu.user.id = :userId")
     Slice<BoardUser> findByUserId(@Param("userId") Long userId);
+
+    List<BoardUser> findAllByBoardId(Long boardId);
 }

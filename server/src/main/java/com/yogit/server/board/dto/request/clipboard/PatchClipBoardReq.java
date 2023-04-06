3f936de27372ceca6665cabfd5dae64ea6c50608
xhaktmchl <xhaktmchl@naver.com>
2023-04-06 14:26:10 +0900

    #9 fix: clipboard수정-req title, introduction, kindofperson길이3000제한 추ㄱ

--- server/src/main/java/com/yogit/server/board/dto/request/clipboard/PatchClipBoardReq.java
+++ server/src/main/java/com/yogit/server/board/dto/request/clipboard/PatchClipBoardReq.java
@@ -23,7 +23,7 @@ public class PatchClipBoardReq {
     @ApiModelProperty(example = "질문이 있습니다.")
     @ApiParam(value = "클립보드 제목", required = false)
     @NotBlank
-    @Size(max = 3000)
+    @Size(max = 1000)
     private String title;
 
     @ApiModelProperty(example = "경복궁역 몇 번 출구인가요?")

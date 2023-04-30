c8972d54b548ef95dee01ed6ae6b3a7f7d7e6e21
xhaktmchl <xhaktmchl@naver.com>
2023-04-30 22:15:33 +0900

    #9 refactor: 보드 조회API-도시 별(1개) 전체 조회 시 카테고리 조건 추가

--- server/src/main/java/com/yogit/server/board/dto/request/GetAllBoardsByCityReq.java
+++ server/src/main/java/com/yogit/server/board/dto/request/GetAllBoardsByCityReq.java
@@ -9,19 +9,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GetAllBoardsByCityReq {
 
-    @ApiModelProperty(example = "1")
-    @ApiParam(value = "유저 ID", required = true)
+    @ApiModelProperty(example = "1", value = "유저 ID")
+    @ApiParam(required = true)
     private Long userId;
 
-    @ApiModelProperty(example = "1")
-    @ApiParam(value = "페이징 조회 페이지", required = true)
+    @ApiModelProperty(example = "1", value = "페이징 조회 페이지")
+    @ApiParam(required = true)
     private int cursor;
 
-    @ApiModelProperty(example = "SEOUL")
-    @ApiParam(value = "City 이름", required = true)
+    @ApiModelProperty(example = "1", value = "그룹 카테고리 ID")
+    @ApiParam( required = true)
+    private Long categoryId;
+
+    @ApiModelProperty(example = "SEOUL", value = "City 이름")
+    @ApiParam(required = true)
     private String cityName;
 
-    @ApiModelProperty(example = "reb5085c395164587b84ac583d023011f.0.sryrq.IDLsECw-rsTozfsX0Yz-CA")
-    @ApiParam(value = "애플 리프레쉬 토큰", required = true)
+    @ApiModelProperty(example = "reb5085c395164587b84ac583d023011f.0.sryrq.IDLsECw-rsTozfsX0Yz-CA", value = "애플 리프레쉬 토큰")
+    @ApiParam(required = true)
     private String refreshToken;
 }
